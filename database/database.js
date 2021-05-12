988aa0647ad3528211ee0eb9d3330098b6005b83
Fellx24 <gavrilov31072003@gmail.com>
2021-05-12 18:17:17 +0700

    авторизация пользователя

--- database/database.js
+++ database/database.js
@@ -1,16 +1,16 @@
 const config = require('config')
-const e = require('express')
 const { Client } = require('pg')
 
-const client = new Client(config.get('DATABASE'))
+const client = new Client(config.get("DATABASE"))
 
 const connect = () => {
-    client.connect(err =>{
-        if (err) {
-            console.error('Database connection error', err.stack)
+    client.connect(err => {
+        if(err) {
+            console.error("Fail connect to database", err.stack)
         } else {
-            console.log('Database connect...')
+            console.log("Database connected...")
         }
     })
 }
-module.exports = {client,connect}
+
+module.exports = {client, connect}
